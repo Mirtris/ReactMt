d85b0864c391ac34ba9ad68d08e04b5f349678bb
mirtris <mirtris0522@gmail.com>
2022-05-09 16:21:35 +0800

    hidden

--- Count256/rcount/src/index.js
+++ Count256/rcount/src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 const {useState} = React;
 
 const Counter = () => {
-    let [count, setCount] = useState(256);
+    let [count, setCount] = useState(10);
 
     return(
         <div className='container' style={shadow}>
-        <div className='chevron chevron-up'
+        <div className='chevron chevron-up' style={{visibility: count >= 10 && 'hidden'}}
         onClick={() => {
             setCount(count+=1);
         }}
         ></div>
         <div className='number'>{count}</div>
-        <div className='chevron chevron-down'
+        <div className='chevron chevron-down' style={{visibility: count <= 0 && 'hidden'}}
         onClick={() =>{
             setCount(count-=1);
         }}
@@ -24,9 +24,10 @@ const Counter = () => {
 } // 單純回傳值時，return可以省略
 
 const shadow = {
-    boxShadow: "0 0 8px 8px #221",
-    padding:23,
+    boxShadow: "0 0 10px 9px #221",
+    padding: 50,
 };
+
 
 ReactDOM.render(
    <Counter />
