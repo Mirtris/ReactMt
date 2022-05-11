ade382771298b89441d844748318708ba65af5b8
mirtris <mirtris0522@gmail.com>
2022-05-11 16:34:27 +0800

    done?

--- Count256/rcount/src/index.js
+++ Count256/rcount/src/index.js
@@ -1,35 +1,34 @@
-import React from 'react';
+import React  from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 const {useState} = React;
 
+let counters = Array.from({length: 4}, (i, index) => index);
 const Counter = () => {
-    let [count, setCount] = useState(10);
+let [count, setCount] = useState(10)
+const clickControl = (type) => () => 
+    setCount(type === 'increase' ? count+1 : count-1);
 
     return(
-        <div className='container' style={shadow}>
-        <div className='chevron chevron-up' style={{visibility: count >= 10 && 'hidden'}}
-        onClick={() => {
-            setCount(count+=1);
-        }}
-        />
-        <div className='number'>{count}</div>
-        {count > 0 &&<div className='chevron chevron-down'
-        onClick={() =>{
-            setCount(count-=1);
-        }}
-        />}
+        <div className="container">
+            {count < 10 && <div className="chevron chevron-up" 
+            onClick={clickControl('increase')}/>}
+            <div className="number">
+            {count}
+            </div>
+            {count > 0 && <div className="chevron chevron-down" 
+            onClick={clickControl('decrease')}/>}
         </div>
     )
-} // 單純回傳值時，return可以省略
-
-const shadow = {
-    boxShadow: "0 0 10px 9px #221",
-    padding: 50,
-};
+}
 
 
-ReactDOM.render(
-   <Counter />
-    , document.getElementById("root")
-)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<div 
+    style={{
+        display:'flex',
+        flexWrap:'wrap',
+
+}}>{counters.map((item) => (<Counter />))}</div>)
